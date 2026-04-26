03addc0719455ac2b7d499bda0f256882e869bc0
zetaraken <42134477+zetaraken@users.noreply.github.com>
2026-04-26 11:15:39 +0900

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+function App(){
+  return <h1>AI매출업 관리자 웹 정상 작동</h1>;
+}
+
+createRoot(document.getElementById("root")).render(<App />);
